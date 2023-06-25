352217ca83ab87f792cabb7410de4250d9d561ca
nguyendang2909 <nguyendang2909@gmail.com>
2023-06-25 14:15:04 +0700

    first commit

--- yt-sharing-ui/src/index.tsx
+++ yt-sharing-ui/src/index.tsx
@@ -1,6 +1,10 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import './styles/main.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
